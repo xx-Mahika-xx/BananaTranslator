4961d595bc1eb42f9d15ab890cbf4b07ea4883c6
Mahika Kushwaha <mahikakushwaha@gmail.com>
2021-09-17 12:37:17 +0300

    created button

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Script works")
+var btnTranslate= document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click",function clickEventHandler()
+{
+    console.log("clicked")
+})
